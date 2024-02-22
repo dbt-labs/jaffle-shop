f3a30f7fadc6652d544c2d6ddd3ebd38082196b7
Dave Connors <dave.connors@fishtownanalytics.com>
2024-02-22 09:04:43 -0600

    remove thing that was broke

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -11,7 +11,7 @@ source as (
     select * from {{ source('ecom', 'raw_orders') }}
     -- if you generate a larger dataset,
     -- you can limit the timespan to the current time with the following line
-    where ordered_at <= {{ var('truncate_timespan_to') }}
+    {# where ordered_at <= {{ var('truncate_timespan_to') }} #}
 
 ),
 
