aad30c150f0c610b16b33764d4abedc8e0214e6a
Winnie Winship <winnie@fishtownanalytics.com>
2024-03-27 15:47:00 +0000

    Fix bugs in truncate_timespan to var

--- models/staging/stg_locations.sql
+++ models/staging/stg_locations.sql
@@ -4,9 +4,6 @@ source as (
 
     select * from {{ source('ecom', 'raw_stores') }}
 
-    -- if you generate a larger dataset,
-    -- you can limit the timespan to the current time with the following line
-    -- where opened_at <= {{ var('truncate_timespan_to') }}
 ),
 
 renamed as (
