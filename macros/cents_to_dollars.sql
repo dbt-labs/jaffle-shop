62ca685cbaaacb56a307f2a97a8766160cf2990f
gwen windflower <gwenwindflower@gmail.com>
2024-03-28 15:21:23 -0500

    Fix bad parens in cents_to_dollars

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,5 +1,5 @@
 {# A basic example for a project-wide macro to cast a column uniformly #}
 
 {% macro cents_to_dollars(column_name, precision=2) -%}
-    (cast({{ column_name }} / 100) as numeric(16, {{ precision }})
+    (cast({{ column_name }} / 100 as numeric(16, {{ precision }})))
 {%- endmacro %}
