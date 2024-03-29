3d5856bfb964698203845b0cfaa1bd180347085c
gwen windflower <gwenwindflower@gmail.com>
2024-03-29 06:09:37 -0500

    Add dispatch for BQ in cents_to_dollars_macro

--- macros/cents_to_dollars.sql
+++ macros/cents_to_dollars.sql
@@ -1,5 +1,13 @@
 {# A basic example for a project-wide macro to cast a column uniformly #}
 
 {% macro cents_to_dollars(column_name) -%}
-    round(cast(({{ column_name }} / 100) as numeric), 2)
+    {{ return(adapter.dispatch('cents_to_dollars')(column_name)) }}
 {%- endmacro %}
+
+{% macro default__cents_to_dollars(column_name) -%}
+    ({{ column_name }} / 100)::numeric(16, 2)
+{%- endmacro %}
+
+{% macro bigquery__cents_to_dollars(column_name) %}
+    round(cast(({{ column_name }} / 100) as numeric), 2)
+{% endmacro %}
