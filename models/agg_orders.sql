e48208b886541f963fb8d5e9490a7c906ccab669
jared-hardinger <114436106+jared-hardinger@users.noreply.github.com>
2025-06-05 17:25:40 -0600

    Update agg_orders.sql

--- models/agg_orders.sql
+++ models/agg_orders.sql
@@ -3,6 +3,6 @@ SELECT
     o.status,
     c.customer_id,
     SUM(o.amount) as total_amount
-FROM {{ ref('orders') }} oAdd commentMore actions
+FROM {{ ref('orders') }} o
 JOIN {{ ref('customers') }} c USING(customer_id)
 GROUP BY ALL
