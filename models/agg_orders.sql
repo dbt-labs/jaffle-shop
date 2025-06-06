7accd4715eb6bb9b7c7dacc7228f6c9934f8d6a2
collectors-test@data.world <collectors-test@data.world>
2025-06-06 18:10:23 +0000

    tweaked agg_orders

--- models/agg_orders.sql
+++ models/agg_orders.sql
@@ -1,7 +1,8 @@
 SELECT
-    o.status,
-    c.customer_id,
-    SUM(o.amount) as total_amount
+      ordered_at::DATE as order_date
+    , customer_id
+    , SUM(order_cost) as total_order_cost
+    , SUM(order_items_subtotal) as total_order_items_subtotal
+    , SUM(count_order_items) as total_order_items_count
 FROM {{ ref('orders') }} o
-JOIN {{ ref('customers') }} c USING(customer_id)
 GROUP BY ALL
