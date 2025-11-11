4e931ee74ab291c7a88682ca410ceb216a8238d3
Mirna Wong <mirna.wong@fishtownanalytics.com>
2025-11-11 15:17:59 +0000

    test

--- functions/is_positive_int.py
+++ functions/is_positive_int.py
@@ -1,1 +1,8 @@
-REGEXP_CONTAINS(a_string, r'^[0-9]+$')
+import re
+
+def main(a_string):
+    """
+    Return 1 if a_string represents a positive integer (e.g., "10", "+8" not allowed here),
+    else 0.
+    """
+    return 1 if re.search(r'^[0-9]+$', a_string or '') else 0
