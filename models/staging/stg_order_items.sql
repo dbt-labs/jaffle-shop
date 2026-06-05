d76fe9837196247f17576da26f5b0fb71d316ffe
Marie-Christine Boyer <mboyer@actinvision.com>
2026-06-05 12:32:41 +0000

    fix sources

--- models/staging/stg_order_items.sql
+++ models/staging/stg_order_items.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('ecom', 'raw_items') }}
+    select * from {{ source('ecom', 'items') }}
 
 ),
 
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('ecom', 'raw_orders') }}
+    select * from {{ source('ecom', 'orders') }}
 
 ),
 
