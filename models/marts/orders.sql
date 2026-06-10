a3543e23eef7707c84cee032f27fef52cc52acf7
muneeb.master@dbtlabs.com <muneeb.master@dbtlabs.com>
2026-06-10 15:25:31 +0000

    Add is_large_order flag to orders for Advanced CI demo

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -50,7 +50,8 @@ compute_booleans as (
         order_items_summary.count_drink_items,
         order_items_summary.count_order_items,
         order_items_summary.count_food_items > 0 as is_food_order,
-        order_items_summary.count_drink_items > 0 as is_drink_order
+        order_items_summary.count_drink_items > 0 as is_drink_order,
+        orders.order_total >= 20 as is_large_order
 
     from orders
 
