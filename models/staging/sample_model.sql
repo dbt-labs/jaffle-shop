1bc522d2b320425b18fa5e20ecd440ef96412e9d
[C] Katerina Arsh <katerina.arsh@dbtlabs.com>
2026-06-16 11:01:20 +0000

    c

--- models/staging/sample_model.sql
+++ models/staging/sample_model.sql
@@ -6,4 +6,4 @@ DATEDIFF ('day',REVIEW_END_DATE,PRODUCTION_START_DATE) AS Review_to_production,
 DATEDIFF ('day',PRODUCTION_START_DATE,PRODUCTION_END_DATE) AS Production_duration,
 DATEDIFF ('day',SUBMISSION_DATE,PUBLICATION_DATE) AS Submission_to_publication,
 
-from {{ ref('seed') }}
+from {{ ref('seed') }}.  
